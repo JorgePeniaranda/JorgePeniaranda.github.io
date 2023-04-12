d2ab2bb0966dbdc6b84f4ea6b3c904969c6b7055
Jorgemaxii25 <jorge.maxii25@gmail.com>
2023-04-12 12:40:23 -0300

    try to fix nav

--- src/sections/contact/contact.jsx
+++ src/sections/contact/contact.jsx
@@ -1,10 +1,22 @@
 import Nav from '../../components/navbar/nav'
 import Points from '../../components/points/points'
+import $ from 'jquery';
+
+
+function asdqawdw() {
+  window.scroll({
+    top: 0, 
+    left: 0, 
+    behavior: 'smooth' 
+   });
+  console.log("Asdas");
+}
 
 const main = () => {
     return (
       <section className="sections bg-color-5" id='Contact'>
         <Nav/>
+        <button onClick={asdqawdw}>asdas</button>
         <Points step={5}/>
       </section>
     );
