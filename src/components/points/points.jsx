161e3c594a30bb02c5a480f0668fe492693442fa
Jorgemaxii25 <jorge.maxii25@gmail.com>
2023-04-23 12:24:46 -0300

    More responsive - img corrections - color corrections - nav vanish

--- src/components/points/points.jsx
+++ src/components/points/points.jsx
@@ -5,15 +5,15 @@ const Points = param => {
     return (
       <Fade right>
         <div id='points' className={(param.lightMode) ? 'light' : ''}>
-          <a href={param.href[0]} aria-label={param.href[0]} className="point" id={(param.step === 1) ? 'active' : ''}>1</a>
+          <a href={param.href[0]} aria-label={param.href[0]} className={((param.step === 1) ? 'active' : '') + " point"}>1</a>
           <div className="line"/>
-          <a href={param.href[1]} aria-label={param.href[1]} className="point" id={(param.step === 2) ? 'active' : ''}>2</a>
+          <a href={param.href[1]} aria-label={param.href[1]} className={((param.step === 2) ? 'active' : '') + " point"}>2</a>
           <div className="line"/>
-          <a href={param.href[2]} aria-label={param.href[2]} className="point" id={(param.step === 3) ? 'active' : ''}>3</a>
+          <a href={param.href[2]} aria-label={param.href[2]} className={((param.step === 3) ? 'active' : '') + " point"}>3</a>
           <div className="line"/>
-          <a href={param.href[3]} aria-label={param.href[3]} className="point" id={(param.step === 4) ? 'active' : ''}>4</a>
+          <a href={param.href[3]} aria-label={param.href[3]} className={((param.step === 4) ? 'active' : '') + " point"}>4</a>
           <div className="line"/>
-          <a href={param.href[4]} aria-label={param.href[4]} className="point" id={(param.step === 5) ? 'active' : ''}>5</a>
+          <a href={param.href[4]} aria-label={param.href[4]} className={((param.step === 5) ? 'active' : '') + " point"}>5</a>
         </div>
       </Fade>
     );
--- src/sections/me/me.jsx
+++ src/sections/me/me.jsx
@@ -1,12 +1,12 @@
 import Nav from '../../components/navbar/nav'
 import Points from '../../components/points/points'
 import './me.scss'
-import Profile from './assets/profilePhoto.png'
-import Wave from './assets/wave.png' // Acá deberia haber un svg
-import Linkedin from './assets/Linkedin.png'
-import StackOverflow from './assets/StackOverflow.png'
-import Instagram from './assets/Instagram.png'
-import Github from './assets/Github.png'
+/* import Profile from './assets/profilePhoto.png' */
+import Wave from './assets/wave.svg'
+import Linkedin from './assets/Linkedin.svg'
+import StackOverflow from './assets/StackOverflow.svg'
+import Instagram from './assets/Instagram.svg'
+import Github from './assets/Github.svg'
 import { Flip, Bounce, Fade } from 'react-reveal'
 import Pulse from 'react-reveal/Pulse';
 
@@ -16,7 +16,7 @@ const Me = props => {
         <Nav href={props.index}/>
         <div className='container'>
           <div id='presentation'>
-            <Bounce top><span><img src={Profile} alt="Jorge Peñaranda" /></span></Bounce>
+            <Bounce top><span><img src="https://i.pinimg.com/564x/5d/32/47/5d3247875e2ca9b0cb7c915172c4cc10.jpg" alt="Jorge Peñaranda" /></span></Bounce>
             <Fade top><p>Hello, I’am <br/>Jorge<br/>Peñaranda<Pulse delay={700} duration={500}><span>Web Developer</span></Pulse></p></Fade>
           </div>
           <div id='media'>
@@ -26,7 +26,11 @@ const Me = props => {
             <Flip right><a href="https://github.com/jorgepeniaranda" target='_blank' rel="noreferrer"><img src={Github} alt="Github_icon" /></a></Flip>
           </div>
           <Points href={props.index} step={1}/>
-          <img src={Wave} alt="wave" id="wave"/>
+          <div id="wave">
+            <svg width="1920" preserveAspectRatio="none" height="178" viewBox="0 0 1920 178" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M-5 82L59.1667 98C123.333 114 251.667 146 380 140.7C508.333 135 636.667 93 765 92.7C893.333 93 1021.67 135 1150 119.3C1278.33 103 1406.67 29 1535 7.29998C1663.33 -14 1791.67 18 1855.83 34L1920 50V178H1855.83C1791.67 178 1663.33 178 1535 178C1406.67 178 1278.33 178 1150 178C1021.67 178 893.333 178 765 178C636.667 178 508.333 178 380 178C251.667 178 123.333 178 59.1667 178H-5V82Z" fill="#0A0A0A"/>
+            </svg>
+          </div>
         </div>
       </section>
     );
